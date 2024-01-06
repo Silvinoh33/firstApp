6e91d27b5af29b3791499bffd8950e6b5e524a0b
Silvinoh <edhorgounon@gmail.com>
2024-01-06 13:26:03 +0100

    filtrage,formulaire,details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // AppComponent,
+    // HomeComponent
+  
+    // HousingLocationComponent
+  
+    // DetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
+ 
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
-export class AppModule { }
+
+export class AppModule {
+  
+ }
